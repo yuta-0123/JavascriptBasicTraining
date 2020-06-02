054ddd7c88eac5ccda73999ec8a771580914a3b3
yuta-0123 <yuta.fukuda.0913@gmail.com>
2020-06-02 23:50:06 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// arg: 高さと底辺の値
+// logic: 面積を計算する
+// return: 面積
+
+function getArea(base,height) {
+  let base = 10;
+  let height = 5;
+  var triangle=1/2*height*base;
+  return triangle
+}
+
+// オブジェクトのインスタンス化
+// 利用したいオブジェクトの中のメソッド
+// 関数を制作(引数,処理,戻す)
+
+// I => month
+// O => season
+
+function getSeason(month){
+  if(month<3){
+    return "春"
+  }
+}
